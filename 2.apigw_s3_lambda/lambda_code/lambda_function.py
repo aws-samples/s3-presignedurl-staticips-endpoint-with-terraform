de545fc7ca48f2e010fddf7b986f806c085568fb
Jun Soung Lee <junslee@amazon.com>
2023-11-29 20:43:48 +0900

    lambda debug

--- 2.apigw_s3_lambda/lambda_code/lambda_function.py
+++ 2.apigw_s3_lambda/lambda_code/lambda_function.py
@@ -13,13 +13,16 @@ s3_client_for_endpoint = boto3.client(
 
 def lambda_handler(event, context):
     request_path = event['path'].split('/')
+    print(request_path)
 
     presigned_url = s3_client_for_endpoint.generate_presigned_url(
         'get_object',
-        Params={'Bucket': request_path[1], 'Key': '/'.join(request_path[2:])},
+        Params={'Bucket': request_path[2], 'Key': '/'.join(request_path[3:])},
         ExpiresIn=3600
     )
 
+    print(presigned_url)
+    
     return {
         'statusCode': 302,
         'headers': {'Location': f'{presigned_url}'}
