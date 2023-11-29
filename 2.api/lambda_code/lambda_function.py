c9afee131920775b586243c4a149fda3c99bc628
Jin, Song <sonjin@amazon.com>
2023-11-29 17:07:20 +0900

    update presigned url lambda

--- 2.api/lambda_code/lambda_function.py
+++ 2.api/lambda_code/lambda_function.py
@@ -1,36 +1,27 @@
 import os
-import json
 import boto3
-import re
 
-# bucket_name = 'examplebucket.s3.poc.spatchcardio.com'
+# bucket_name = 'example-endpoint.apg-scratchers.store'
 bucket_name = os.environ['BUCKET_NAME']
 endpoint_url = 'https://' + bucket_name
-s3_client = boto3.client('s3', config=boto3.session.Config(signature_version='s3v4',))
+s3_client = boto3.client('s3', config=boto3.session.Config(signature_version='s3v4', ))
 s3_client_for_endpoint = boto3.client(
-            service_name='s3',
-            endpoint_url=endpoint_url
-        )
-    
-regex_pattern = r'\/'
-        
-def lambda_handler(event, context):
-    test_id = event['pathParameters']['testId']
-    prefix = f'{test_id}/'
-    objects = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    service_name='s3',
+    endpoint_url=endpoint_url
+)
 
-    presigned_url = ''
-    for obj in objects.get('Contents', []):
-        if obj['Key'].endswith('.pdf'):
-            obj_path = re.split(regex_pattern, obj['Key'], 1)
-            presigned_url = s3_client_for_endpoint.generate_presigned_url(
-                'get_object',
-                Params={'Bucket': obj_path[0], 'Key': obj_path[1]},
-                ExpiresIn=3600
-            )
-            break
-        
+
+def lambda_handler(event, context):
+    request_path = event['path'].split('/')
+
+    presigned_url = s3_client_for_endpoint.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': request_path[1], 'Key': '/'.join(request_path[2:])},
+        ExpiresIn=3600
+    )
+
     return {
         'statusCode': 302,
-        'headers' : {'Location': f'{presigned_url}'}
+        'headers': {'Location': f'{presigned_url}'}
     }
+
